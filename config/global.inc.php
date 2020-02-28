8e3757001a8eac1fbe8dc122a0f49c3f5c6c8e62
Alberto <pruebas@mail.com>
2020-02-28 20:19:25 +0100

    Versión para Clever Cloud

--- config/global.inc.php
+++ config/global.inc.php
@@ -5,7 +5,13 @@
 /***********************************************************************/
 
 // Conexión a la base de datos
-define('HOST_NAME', 'localhost');
+/*define('HOST_NAME', 'localhost');
 define('USERNAME', 'root');
 define('PASSWORD', '');
-define('DATABASE', 'registar');
+define('DATABASE', 'registar');*/
+
+// Configuración para Clever Cloud
+define('HOST_NAME', 'bgzj641s2o4y8qfnl5sb-mysql.services.clever-cloud.com');
+define('USERNAME', 'u4skyhniu5yskhai');
+define('PASSWORD', 'u0nVqNTwAUCxFaJ9sW8s');
+define('DATABASE', 'bgzj641s2o4y8qfnl5sb');
